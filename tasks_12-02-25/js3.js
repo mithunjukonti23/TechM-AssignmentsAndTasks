b4e3b30fb72fe72328991d819e17dcfa512605e0
Jukonti Mithun Reddy <123626644+mithunjukonti23@users.noreply.github.com>
2025-02-13 00:39:58 +0530

    Add files via upload

---
+++ tasks_12-02-25/js3.js
@@ -0,0 +1,7 @@
+//area of triangle
+var a=6;
+var b=5;
+var c=7;
+var s=(a+b+c)/2;
+var area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+console.log(area)
---
+++ tasks_12-02-25/js5.js
@@ -0,0 +1,6 @@
+//get current date
+var date = new Date();
+var d= date.getDate();
+var m= date.getMonth() + 1;
+var y = date.getFullYear();
+console.log(d + "-" + m + "-" + y);
