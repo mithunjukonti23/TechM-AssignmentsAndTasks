fe2c4e6026b3c87f169deac75164462d4d6c0a3d
Jukonti Mithun Reddy <123626644+mithunjukonti23@users.noreply.github.com>
2025-02-13 00:42:30 +0530

    Delete tasks_12-02-25/js6.js

--- tasks_12-02-25/js6.js
+++
@@ -1,6 +0,0 @@
-//sorting numbers in descending order
-function sortNumbers(a, b, c) {
-    let numbers = [a, b, c].sort((x, y) => y - x);
-    console.log(numbers.join(','));
-}
-sortNumbers(0, -1, 4);
